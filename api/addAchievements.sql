5dbeb60ae7cc3d524000462f0328ef6361896852
Daniel Brierley <daniel@thebrierleys.co.uk>
2022-03-13 22:59:31 +0000

    page update

--- api/addAchievements.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO achievements (name, description)
-VALUES
-('Achievement3', 'This is a test 5'),
-('Achievement4', 'This is a test 6')
--- api/addQuestions.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO questions (question, theme, ans1, ans2, ans3) 
-VALUES ("Which planet is closest to the sun?", "SolarSystem", "Mercury", "Saturn", "Uranus"),
-("What is the biggest planet in our solar system?", "SolarSystem", "Jupiter", "Saturn", "Venus"),
-("How many planets does our solar system consist of?", "SolarSystem", "8", "10", "12"),
-("How many gas giants are there in the solar system?", "SolarSystem", "4", "9", "10"),
-("Which planet has the highest atmopheric temperature?", "SolarSystem", "Venus", "Mercury", "Earth");
-
-
